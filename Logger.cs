cacea4055dc7d1e01a45a3ca82dbbd58d5213255
Adilson <usadilsondigital@gmail.com>
2024-02-01 14:45:36 -0600

    class logger

---
+++ Logger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_CustomInterpolationHandler
+{
+    public class Logger
+    {
+        public LogLevel EnabledLevel { get; init; } = LogLevel.Error;
+
+        public void LogMessage(LogLevel level, string msg)
+        {
+            if (EnabledLevel < level) return;
+            Console.WriteLine(msg);
+        }
+
+    }
+}
