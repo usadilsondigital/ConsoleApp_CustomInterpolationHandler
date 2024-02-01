c77e77b00c514c75d82369f3a6fa18337fa23640
Adilson <usadilsondigital@gmail.com>
2024-02-01 15:48:06 -0600

    logger message

--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var logger = new Logger() { EnabledLevel = LogLevel.Warning };
+            var time = DateTime.Now;
+
+            logger.LogMessage(LogLevel.Error, $"Error Level. CurrentTime: {time}. This is an error. It will be printed.");
+            logger.LogMessage(LogLevel.Trace, $"Trace Level. CurrentTime: {time}. This won't be printed.");
+            logger.LogMessage(LogLevel.Warning, "Warning Level. This warning is a string, not an interpolated string expression.");
         }
     }
 }
