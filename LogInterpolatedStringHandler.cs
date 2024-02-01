cf577ae3aaabd250b7044cb8909944409c4d749f
Adilson <usadilsondigital@gmail.com>
2024-02-01 14:53:07 -0600

    struct loiInterpolatedstringhandler

---
+++ LogInterpolatedStringHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_CustomInterpolationHandler
+{
+
+    [InterpolatedStringHandler]
+    public ref struct LogInterpolatedStringHandler
+    {
+        // Storage for the built-up string
+        StringBuilder builder;
+
+        public LogInterpolatedStringHandler(int literalLength, int formattedCount)
+        {
+            builder = new StringBuilder(literalLength);
+            Console.WriteLine($"\tliteral length: {literalLength}, formattedCount: {formattedCount}");
+        }
+
+      
+
+
+    }
+}
